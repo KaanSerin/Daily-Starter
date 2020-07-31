74a78bcaf40ab30ad6ee98e6a17688dba9904010
Kaan Serin <kaanserin113@gmail.com>
2020-07-31 23:23:17 +0300

    tweeked some stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,70 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
+import axios from "axios";
 import "./App.css";
 import WeatherSection from "./components/WeatherSection/WeatherSection";
+import { ForecastContext } from "./context/ForecastContext";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const { forecastData, setForecastData } = useContext(ForecastContext);
+
+  useEffect(() => {
+    const search = localStorage.getItem("search");
+    if (search !== null) {
+      getForecast(search);
+    }
+  }, []);
+
+  const getForecast = (city) => {
+    setIsLoading(true);
+    axios
+      .get(
+        `http://api.openweathermap.org/data/2.5/forecast?q=${city}&mode=json&units=metric&appid=ea68e365d06e5d2f65aaf446623175b1`
+      )
+      .then((response) => {
+        axios
+          .get(
+            `https://api.openweathermap.org/data/2.5/onecall?lat=${response.data.city.coord.lat}&cnt=7&units=metric&lon=${response.data.city.coord.lon}&appid=ea68e365d06e5d2f65aaf446623175b1`
+          )
+          .then((response) => {
+            setForecastData(formatWeatherData({ ...response.data, city }));
+            setIsLoading(false);
+          })
+          .catch((err) => {
+            console.log(err);
+            setIsLoading(false);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(false);
+      });
+  };
+
+  const formatWeatherData = (data) => {
+    console.log(data);
+    const result = data.daily.map((day) => ({
+      city: data.city,
+      day: new Date(1000 * day.dt),
+      temp: {
+        day: day.temp.day,
+        night: day.temp.night,
+      },
+      humidity: day.humidity,
+      weather: {
+        description: day.weather[0].description,
+        icon: day.weather[0].icon,
+      },
+    }));
+
+    return result;
+  };
+
   return (
     <div className="App">
-      <WeatherSection />
+      <Navbar getForecast={getForecast} />
+      <WeatherSection isLoading={isLoading} />
     </div>
   );
 }
--- src/components/WeatherSection/WeatherSection.js
+++ src/components/WeatherSection/WeatherSection.js
@@ -1,65 +1,10 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import WeatherList from "./WeatherList/WeatherList";
 import { ForecastContext } from "../../context/ForecastContext";
 import Spinner from "../Spinner/Spinner";
-import Navbar from "../Navbar/Navbar";
-import axios from "axios";
 
-const WeatherSection = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const { forecastData, setForecastData } = useContext(ForecastContext);
-
-  useEffect(() => {
-    const search = localStorage.getItem("search");
-    if (search !== null) {
-      getForecast(search);
-    }
-  }, []);
-
-  const getForecast = (city) => {
-    setIsLoading(true);
-    axios
-      .get(
-        `http://api.openweathermap.org/data/2.5/forecast?q=${city}&mode=json&units=metric&appid=ea68e365d06e5d2f65aaf446623175b1`
-      )
-      .then((response) => {
-        axios
-          .get(
-            `https://api.openweathermap.org/data/2.5/onecall?lat=${response.data.city.coord.lat}&cnt=7&units=metric&lon=${response.data.city.coord.lon}&appid=ea68e365d06e5d2f65aaf446623175b1`
-          )
-          .then((response) => {
-            setForecastData(formatWeatherData({ ...response.data, city }));
-            setIsLoading(false);
-          })
-          .catch((err) => {
-            console.log(err);
-            setIsLoading(false);
-          });
-      })
-      .catch((err) => {
-        console.log(err);
-        setIsLoading(false);
-      });
-  };
-
-  const formatWeatherData = (data) => {
-    console.log(data);
-    const result = data.daily.map((day) => ({
-      city: data.city,
-      day: new Date(1000 * day.dt),
-      temp: {
-        day: day.temp.day,
-        night: day.temp.night,
-      },
-      humidity: day.humidity,
-      weather: {
-        description: day.weather[0].description,
-        icon: day.weather[0].icon,
-      },
-    }));
-
-    return result;
-  };
+const WeatherSection = ({ isLoading }) => {
+  const { forecastData } = useContext(ForecastContext);
 
   let weatherList = null;
 
@@ -78,7 +23,6 @@ const WeatherSection = () => {
 
   return (
     <div className="">
-      <Navbar getForecast={getForecast} />
       {header}
       {weatherList}
     </div>
