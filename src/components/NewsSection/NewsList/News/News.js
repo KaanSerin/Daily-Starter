c69d00c6644db27e1e2ebdf6275e6e66e079eaae
Kaan Serin <kaanserin113@gmail.com>
2020-08-03 15:22:37 +0300

    added Material ui button to the news component.

--- src/components/NewsSection/NewsList/News/News.js
+++ src/components/NewsSection/NewsList/News/News.js
@@ -1,14 +1,31 @@
 import React from "react";
 import classes from "./News.module.css";
 import newsIcon from "../../../../assets/images/defaultNews.jpg";
+import { Button, createMuiTheme, MuiThemeProvider } from "@material-ui/core";
 
 const News = ({ title, description, imageUrl, url }) => {
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: "#e8505b",
+      },
+      secondary: {
+        main: "#f9d56e",
+      },
+    },
+  });
+
   return (
     <div className={classes.News}>
       <img src={imageUrl ? imageUrl : newsIcon} alt={title} />
       <h3>{title}</h3>
       <p>{description}</p>
-      <a href={url}>Go to the page</a>
+
+      <MuiThemeProvider theme={theme}>
+        <Button color="secondary" variant="contained" href={url}>
+          Go to the page
+        </Button>
+      </MuiThemeProvider>
     </div>
   );
 };
